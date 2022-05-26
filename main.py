3ccea2ac7f856b82ff70d6de35402c34dc659c32
bahatiphill <gur79tub@gmail.com>
2022-05-26 15:53:31 +0200

    Call it a day

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-from typing import Union
+from enum import Enum
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+class ModelName(str, Enum):
+    alexnet =  "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
-app =  FastAPI()
+fake_db_items = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{id}")
-def read_item(id:int, q:Union[str, None] = None):
-    return {"id": id, "q": q}
+async def root():
+    return {"message": "Hello World"}
 
 
-@app.put("/items/{id}")
-def update_item(id: int, item:Item):
-    return {"name": item.name, "id":id}
+@app.get("/items")
+async def read_item_2(skip: int=0, limit: int =10):
+    return fake_db_items[skip : limit+skip]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int)-> dict:
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message":"Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message":"LeCNN all the images"}
+
+    return {"model_name": model_name, "message":"Have some residuals"}
